f6e19ccd000321964b5f5b3b70a6c94b883e45ff
AzkaLufthansa <azkalufthansa3@gmail.com>
2022-04-18 13:13:44 +0700

    Membuat view home

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header class="container mt-5">
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
+        <h1 class="d-inline">Hotel Hebat</h1>
+    
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kamar</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Fasilitas</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="container mt-4 mb-5 p-5 border">
+     
+    </div>
+</header>

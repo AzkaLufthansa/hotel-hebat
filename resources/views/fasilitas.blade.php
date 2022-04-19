098e23e4e8ff013401aeaaa9c6617fb130d9a663
AzkaLufthansa <azkalufthansa3@gmail.com>
2022-04-19 08:57:39 +0700

    Menambahkan gambar fasilitas hotel

--- resources/views/fasilitas.blade.php
+++ resources/views/fasilitas.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-    <h2 class="mb-4">Fasilitas</h2>
+<h2 class="mb-4">Fasilitas</h2>
 
-    <div class="row">
-        <div class="cotainer p-5 col-md-4 border"></div>
-        <div class="cotainer p-5 col-md-4 border"></div>
-        <div class="cotainer p-5 col-md-4 border"></div>
+<div class="row justify-content-center">
+        <div class="fasilitas fasilitas-1 me-5 col-md border border-dark"></div>
+        <div class="fasilitas fasilitas-2 me-5 col-md border border-dark"></div>
+        <div class="fasilitas fasilitas-3 me-5 col-md border border-dark"></div>
     </div>
 @endsection

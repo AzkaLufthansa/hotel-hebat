b8029585ef1fe23c48d3e221ba3c7f37d90f9aa5
AzkaLufthansa <azkalufthansa3@gmail.com>
2022-04-28 11:24:20 +0700

    Memperbaiki tampilan home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
-<header class="container mt-5">
+<header class="container my-5">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center">
-        <h1 class="d-inline">Hotel Hebat</h1>
+        <h1>Hotel Hebat</h1>
     
         <ul class="nav justify-content-end">
             <li class="nav-item">
@@ -15,32 +15,8 @@
         </ul>
     </div>
 
-    <div class="hero mt-4 mb-5 border border-dark"></div>
-
-    {{-- <div id="carouselExampleControls" class="carousel slide mt-4 mb-5 border border-dark border-1" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="0" class="active"></button>
-            <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="1"></button>
-            <button type="button" data-bs-target="#carouselExampleControls" data-bs-slide-to="2"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="/img/hotel.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="/img/hotel4.jpg" class="d-block w-100" alt="...">
-            </div>
-            <div class="carousel-item">
-                <img src="/img/hotel5.jpg" class="d-block w-100" alt="...">
-            </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div> --}}
+    {{-- <div class="hero mt-4 border border-dark"></div> --}}
+    <div class="hero mt-4 border border-dark">
+        <img src="/img/hotel.jpg" alt="Hotel Hebat">
+    </div>
 </header>

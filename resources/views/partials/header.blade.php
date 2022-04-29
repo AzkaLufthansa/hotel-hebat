6cae0caafc1fbf12e9988024222e2ab2b6ed82f6
AzkaLufthansa <azkalufthansa3@gmail.com>
2022-04-29 09:16:16 +0700

    Menambahkan modal logout

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -16,10 +16,7 @@
 
             @auth
             <li class="nav-item">
-                <form action="/logout" method="POST">
-                    @csrf
-                    <button type="submit" class="nav-link logout-button" onclick="return confirm('Logout?')">Logout</button>
-                </form>
+                <a class="nav-link" href="/logout" data-bs-toggle="modal" data-bs-target="#logoutModal">Logout</a>
             </li>
             @else
             <li class="nav-item">
@@ -35,3 +32,28 @@
         <img src="/img/hotel.jpg" alt="Hotel Hebat">
     </div>
 </header>
+
+
+
+
+{{-- Modal Box Logout --}}
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="logoutModalLabel">Logout</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Apakah anda yakin untuk logout?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+                <form action="/logout" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Logout</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
